7502b284ef68a8c57abcf0f3ffb452c8657ce6b2
Wrkngmn <Wrkngmn@gmail.com>
2025-07-10 11:38:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+ document.addEventListener('DOMContentLoaded', function () {
   const snowData = {
     "Southcentral": {
       "Anchorage": 4,
